c37fc748836eee67f3768b4ed611c789929bd113
HanSolo1000Falcon <HanSolo1000Falcon.business@gmail.com>
2025-06-25 21:37:54 +0200

    first commit

---
+++ MainMod.cs
@@ -0,0 +1,12 @@
+﻿using CapuchinMelonLoaderTemplate;
+using MelonLoader;
+
+[assembly : MelonInfo(typeof(MainMod), ModInfo.NAME, ModInfo.VERSION, ModInfo.AUTHOR)]
+
+namespace CapuchinMelonLoaderTemplate
+{
+    public class MainMod : MelonMod
+    {
+        
+    }
+}
---
+++ ModInfo.cs
@@ -0,0 +1,9 @@
+namespace CapuchinMelonLoaderTemplate
+{
+    public class ModInfo
+    {
+        public const string VERSION = "1.0.0";
+        public const string NAME = "MODNAMEREPLACEMEPLEASE";
+        public const string AUTHOR = "AUTHORNAMEREPLACEMEPLEASE";
+    }
+}
